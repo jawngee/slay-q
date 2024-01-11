20ed72a3603cb49bae1499d462a82025d9c35c68
Slay Pics <jon@slay.pics>
2024-01-11 12:29:53 +0700

    - Package desciptions - Exported types

--- packages/slay-q/src/index.ts
+++ packages/slay-q/src/index.ts
@@ -1,6 +1,7 @@
 export * from './types/rpc';
+export * from './types/events';
+export { SlayQNoRetryError } from './types/errors';
 export * from './driver';
 export * from './driver/supabase';
 export * from './function';
-export { SlayQNoRetryError } from './types/errors';
 export * from './client';
